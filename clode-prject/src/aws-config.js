d5de1ab6ec8a1d8a2ca1a76b04f5c39a884d2421
Yousef Najeh <92521652+yousef-najeh@users.noreply.github.com>
2025-05-18 15:09:39 +0300

    Lex chat bot

---
+++ clode-prject/src/aws-config.js
@@ -0,0 +1,16 @@
+import { LexRuntimeV2Client } from "@aws-sdk/client-lex-runtime-v2";
+
+// Replace these with your actual AWS credentials
+const awsConfig = {
+  region: "us-east-1", 
+  credentials: {
+    accessKeyId: "AKIAUULKJGIMAFUGDIWR",
+    secretAccessKey: "fTGEZdN1O0M3k1l3tZWA6H3hiSavdBsyr7y5ZMbr"
+  }
+};
+
+export const lexClient = new LexRuntimeV2Client(awsConfig);
+export const botId = "P0CWNCGHKK";
+
+export const botAliasId = "LBX0BAAYL3";
+export const localeId = "en_US"; 
